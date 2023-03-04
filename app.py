188b7476f994a8c5e5d8b893e29f6533840000d8
Julia Melchert <melcherj@flip1.engr.oregonstate.edu>
2023-03-04 00:46:43 -0800

    Refactored all code in app.py and queries.py (except for edit route in app.py)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 import database.db_connector as db
-import queries
+from queries import *
 import os
 
 # Configuration
@@ -16,50 +16,50 @@ def root():
 def album_reviews():
     """ Displays the Album_Reviews table """
 
-    return render_template("entities/album_reviews.html", album_reviews=queries.get_all_album_reviews())
+    return render_template("entities/album_reviews.html", album_reviews=get_all_album_reviews())
 
 @app.route('/song-reviews', methods=['GET', 'POST'])
 def song_reviews():
     """ Displays the Song_Reviews table """
 
-    return render_template("entities/song_reviews.html", song_reviews=queries.get_all_song_reviews())
+    return render_template("entities/song_reviews.html", song_reviews=get_all_song_reviews())
 
 @app.route('/albums', methods=['GET', 'POST'])
 def albums():
     """ Displays the Albums table """
 
-    return render_template("entities/albums.html", albums=queries.get_all_albums())
+    return render_template("entities/albums.html", albums=get_all_albums())
 
 @app.route('/albums-songs', methods=['GET', 'POST'])
 def albums_songs():
     """ Displays the Albums_Songs table """
 
-    return render_template("entities/albums_songs.html", albums_songs=queries.get_all_albums_songs())
+    return render_template("entities/albums_songs.html", albums_songs=get_all_albums_songs())
 
 @app.route('/artists', methods=['GET', 'POST'])
 def artists():
     """ Displays the Artists table """
     
-    return render_template("entities/artists.html", artists=queries.get_all_artists)
+    return render_template("entities/artists.html", artists=get_all_artists())
 
 @app.route('/songs', methods=['GET', 'POST'])
 def songs():
     """ Displays the Songs table """
 
-    return render_template("entities/songs.html", songs=queries.get_all_songs())
+    return render_template("entities/songs.html", songs=get_all_songs())
 
 @app.route('/users', methods=['GET', 'POST'])
 def users():
     """ Displays the Users table """
 
-    return render_template("entities/users.html", users=queries.get_all_users())
+    return render_template("entities/users.html", users=get_all_users())
 
 @app.route('/add/<review_type>-review', methods=['GET', 'POST'])
 def add_review(review_type):
     """ Handles creations of Song_Reviews and Album_Reviews """
 
     if request.method == "GET":
-        results = queries.get_add_review_info(review_type)
+        results = get_add_review_info(review_type)
 
     elif request.method == "POST":
         return redirect(url_for(review_type + "_reviews"))
@@ -70,39 +70,41 @@ def add_review(review_type):
 def add_entity(entity_name):
     """ Handles creations of Songs, Albums, Artists, Users, and Albums_Songs """
 
-    # Handle GET requests (loading page)
+    # Handle GET requests from loading the page
     if request.method == "GET":
         if entity_name == "albums_song":
-            results = queries.get_star_albums_songs()
+            results = get_star_albums_songs()
         else:
-            results = queries.get_star_entity(entity_name)
+            results = get_star_entity(entity_name)
 
         # Dynamically generates drop-down menus for different entities
         if entity_name == "song" or entity_name == "album":
-            return render_template("add_entity.html", entity_results=results, entity_name=entity_name, fk_data_1=[], fk_data_2=queries.get_artist_names())
+            return render_template("add_entity.html", entity_results=results, entity_name=entity_name, fk_data_1=[], fk_data_2=get_artist_names())
         elif entity_name == "albums_song":
-            return render_template("add_entity.html", entity_results=results, entity_name=entity_name, fk_data_1=queries.get_album_titles(), fk_data_2=queries.get_song_titles())
+            return render_template("add_entity.html", entity_results=results, entity_name=entity_name, fk_data_1=get_album_titles(), fk_data_2=get_song_titles())
         else:
             return render_template("add_entity.html", entity_results=results, entity_name=entity_name, fk_data_1=[], fk_data_2=[])
 
     # This if-statement section is adapted from the official Flask tutorial (https://flask.palletsprojects.com/en/2.2.x/tutorial/)
+    # Handle POST requests from adding an entity
     elif request.method == "POST":
+        # Insert into Users table
         if entity_name == "user":
-            db.execute_query('INSERT INTO Users (username, email) VALUES (%s, %s)', (request.form['username'], request.form['email']))
+            add_user(request.form['username'], request.form['email'])
             return redirect(url_for("users"))
         
+        # Insert into Albums_Songs table
         elif entity_name == "albums_song":
-            album_id = int(db.execute_query('SELECT album_id FROM Albums WHERE album_title = (%s)', (request.form['fk_data_1'],)).fetchone()['album_id'])
-            song_id = int(db.execute_query('SELECT song_id FROM Songs WHERE song_title = (%s)', (request.form['fk_data_2'],)).fetchone()['song_id'])
+            album_id = get_album_id_from_title(request.form['fk_data_1'])
+            song_id = get_song_id_from_title(request.form['fk_data_2'])
 
-            # Check if the entry already exists, since Albums_Songs additions must be unique
-            exist_val = list((db.execute_query('SELECT EXISTS(SELECT * FROM Albums_Songs WHERE album_id = ' + str(album_id) + ' AND song_id = ' + str(song_id) + ')').fetchone()).values())[0]
-            print(exist_val)
-            if exist_val == 1:
-                flash("This Albums_Song already exists.")
+            # If the entry already exists, an error is flashed since Albums_Songs additions must be unique
+            if check_albums_song_exists(album_id, song_id) == 1:
+                flash("This Albums_Songs entry already exists.")
                 return redirect(url_for("add_entity", entity_name=entity_name))
+            # If the Albums_Songs entry doesn't already exist, it's added
             else:
-                db.execute_query('INSERT INTO Albums_Songs (album_id, song_id) VALUES (%s, %s)', (int(album_id), int(song_id)))
+                add_albums_song(album_id, song_id)
                 return redirect(url_for("albums_songs"))
     
 @app.route('/edit/<entity_name>/<int:entity_id>', methods=('GET', 'POST'))
@@ -113,20 +115,16 @@ def edit(entity_name, entity_id):
     if request.method == "POST":
         if entity_name == "user":
             db.execute_query(f'UPDATE Users SET username = %s, email = %s WHERE user_id = %s', (request.form['username'], request.form['email'], entity_id))
-            return redirect(url_for("users"))
 
         elif entity_name == "albums_song":
             album_id = db.execute_query('SELECT album_id FROM Albums WHERE album_title = (%s)', (request.form['album_fk_data'],)).fetchone()['album_id']
             song_id = db.execute_query('SELECT song_id FROM Songs WHERE song_title = (%s)', (request.form['song_fk_data'],)).fetchone()['song_id']
-            db.execute_query(f'UPDATE Albums_Songs SET album_id = %s, song_id = %s WHERE albums_song_id = %s', (album_id, song_id, entity_id))
-            return redirect(url_for("albums_songs"))
-        
-        else:
-            return redirect(url_for(entity_name + "s"))
+            db.execute_query(f'UPDATE Albums_Songs SET album_id = %s, song_id = %s WHERE albums_song_id = %s', (album_id, song_id, entity_id))        
 
-    # Handle GET requests (loading page)
+        return redirect(url_for(entity_name + "s"))
+
+    # Handle GET requests from loading the page
     elif request.method == "GET":
-        print(entity_name)
         # TODO: Refactor below since it is reused in add_entity path
         if entity_name == "albums_song":
             entity_name = "Albums_Song"
@@ -137,17 +135,17 @@ def edit(entity_name, entity_id):
         else:
             entity_name = entity_name.capitalize()
 
-        results = db.execute_query(f"SELECT * FROM {entity_name}s WHERE {entity_name}_id = {entity_id}").fetchall()
+        results = get_star_entity_where_id(entity_name, entity_id)
 
         if entity_name == "Song" or entity_name == "Album":
-            artist_results = db.execute_query("SELECT name FROM Artists;").fetchall()
+            artist_results = get_artist_names()
 
             artist_id = db.execute_query("SELECT artist_id FROM " + entity_name + "s WHERE " + entity_name.lower() + "_id = %s", (entity_id,)).fetchone()['artist_id']
             current_artist = db.execute_query("SELECT name FROM Artists WHERE artist_id = %s;", (artist_id,)).fetchone()['name']
             return render_template("edit.html", entity_results=results, artist_fk_data=artist_results, current_artist=current_artist, album_fk_data=[], current_album="", entity_name=entity_name.lower())
         elif entity_name == "Albums_Song":
-            song_results = db.execute_query("SELECT song_title FROM Songs;").fetchall()
-            album_results = db.execute_query("SELECT album_title FROM Albums;").fetchall()
+            song_results = get_song_titles()
+            album_results = get_album_titles()
 
             song_id = db.execute_query("SELECT song_id FROM Albums_Songs WHERE albums_song_id = %s", (entity_id,)).fetchone()['song_id']
             album_id = db.execute_query("SELECT album_id FROM Albums_Songs WHERE albums_song_id = %s", (entity_id,)).fetchone()['album_id']
@@ -155,8 +153,8 @@ def edit(entity_name, entity_id):
             current_album = db.execute_query("SELECT album_title FROM Albums WHERE album_id = %s", (album_id,)).fetchone()['album_title']
             return render_template("edit.html", entity_results=results, artist_fk_data=song_results, current_artist=current_song, album_fk_data=album_results, current_album=current_album, entity_name=entity_name.lower())
         elif entity_name == "Album_Review":
-            user_results = db.execute_query("SELECT username FROM Users;").fetchall()
-            album_results = db.execute_query("SELECT album_title FROM Albums;").fetchall()
+            user_results = get_usernames()
+            album_results = get_album_titles()
 
             user_id = db.execute_query("SELECT user_id FROM Album_Reviews WHERE album_review_id = %s", (entity_id,)).fetchone()['user_id']
             album_id = db.execute_query("SELECT album_id FROM Album_Reviews WHERE album_review_id = %s", (entity_id,)).fetchone()['album_id']
@@ -164,8 +162,8 @@ def edit(entity_name, entity_id):
             current_album = db.execute_query("SELECT album_title FROM Albums WHERE album_id = %s", (album_id,)).fetchone()['album_title']
             return render_template("edit.html", entity_results=results, artist_fk_data=user_results, current_artist=current_user, album_fk_data=album_results, current_album=current_album, entity_name=entity_name.lower())
         elif entity_name == "Song_Review":
-            user_results = db.execute_query("SELECT username FROM Users;").fetchall()
-            song_results = db.execute_query("SELECT song_title FROM Songs;").fetchall()
+            user_results = get_usernames()
+            song_results = get_song_titles()
 
             user_id = db.execute_query("SELECT user_id FROM Song_Reviews WHERE song_review_id = %s", (entity_id,)).fetchone()['user_id']
             song_id = db.execute_query("SELECT song_id FROM Song_Reviews WHERE song_review_id = %s", (entity_id,)).fetchone()['song_id']
@@ -180,17 +178,13 @@ def edit(entity_name, entity_id):
 def delete(entity_name, entity_id):
     """ Handles deletions from any of the tables """
 
-    print(f"Deleting a {entity_name}")
     if request.method == "POST":
         if entity_name == "user":
-            db.execute_query(f'DELETE FROM Users WHERE user_id = %s', [entity_id])
-            return redirect(url_for(entity_name.lower() + "s"))
+            delete_user(entity_id)
         elif entity_name == "albums_song":
-            db.execute_query(f'DELETE FROM Albums_Songs WHERE albums_song_id = %s', [entity_id])
-            return redirect(url_for(entity_name.lower() + "s"))
+            delete_albums_song(entity_id)
 
-
-    return redirect(url_for(entity_name.lower() + 's'))
+    return redirect(url_for(entity_name + 's'))
 
 # Listener
 if __name__ == "__main__":
--- queries.py
+++ queries.py
@@ -1,71 +1,157 @@
 import database.db_connector as db
 
+##################################################
+#                                                #
+# REVIEW QUERIES                                 #
+#                                                #
+##################################################
+
+##### ALBUM REVIEWS ######
 def get_all_album_reviews():
-    """ Returns all rows from the Album_Reviews table. """
+    """ Returns aliased information about all Album_Reviews """
     return db.execute_query(("SELECT Album_Reviews.album_review_id AS 'Album Review ID', Albums.album_title AS Album, Users.username AS User, album_rating AS Rating, album_review_body AS Review FROM Album_Reviews"
                              " JOIN Users ON Users.user_id = Album_Reviews.user_id"
                              " JOIN Albums ON Albums.album_id = Album_Reviews.album_id"
                              " ORDER BY Albums.album_title ASC;")).fetchall()
 
+##### SONG REVIEWS ######
 def get_all_song_reviews():
-    """ Returns all rows from the Song_Reviews table. """
+    """ Returns aliased information about all Song_Reviews """
     return db.execute_query(("SELECT Song_Reviews.song_review_id AS 'Song Review ID', Songs.song_title AS Song, Users.username AS User, song_rating AS Rating, song_review_body AS Review FROM Song_Reviews"
                              " JOIN Users ON Users.user_id = Song_Reviews.user_id"
                              " JOIN Songs ON Songs.song_id = Song_Reviews.song_id"
                              " ORDER BY Songs.song_title ASC;")).fetchall()
 
+def get_add_review_info(review_type) -> tuple:
+    """
+    Returns all album/song titles (depending on review_type) and usernames as a tuple,
+        where titles are stored at the 0th index and usernames are stored at the 1st index
+    """
+    reviews = db.execute_query(f"SELECT {review_type}_title FROM {review_type.capitalize()}s;").fetchall()
+    users = db.execute_query("SELECT username FROM Users")
+
+    return (reviews, users)
+
+##################################################
+#                                                #
+# ALBUM QUERIES                                  #
+#                                                #
+##################################################
+
 def get_all_albums():
-    """  """
+    """ Returns aliased information about all Albums """
     return db.execute_query(("SELECT Albums.album_id AS 'Album ID', Albums.album_title AS Album, Artists.name AS Artist, Albums.album_genre AS Genre, Albums.avg_album_rating AS 'Average Rating' FROM Albums"
                              " JOIN Artists ON Artists.artist_id = Albums.artist_id"
                              " ORDER BY Albums.album_title ASC;")).fetchall()
 
 def get_album_titles():
-    """  """
+    """ Returns all album_title values from the Albums table """
     return db.execute_query("SELECT album_title FROM Albums;").fetchall()
 
+def get_album_id_from_title(title) -> int:
+    """ Returns the album_id that corresponds to the given album_title """
+    return int(db.execute_query(f"SELECT album_id FROM Albums WHERE album_title = ('{title}')").fetchone()['album_id'])
+
+##################################################
+#                                                #
+# ALBUMS_SONGS QUERIES                           #
+#                                                #
+##################################################
+
 def get_all_albums_songs():
-    """  """
+    """ Returns aliased information about all Albums_Songs """
     return db.execute_query(("SELECT Albums_Songs.albums_song_id AS 'Album Song ID', Albums.album_title AS Album, Songs.song_title AS Song FROM Albums_Songs"
                              " LEFT JOIN Albums ON Albums.album_id = Albums_Songs.album_id"
                              " LEFT JOIN Songs ON Songs.song_id = Albums_Songs.song_id"
                              " ORDER BY Albums_Songs.albums_song_id ASC;")).fetchall()
 
 def get_star_albums_songs():
-    """  """
+    """ Returns all information from the Albums_Songs table, without aliases """
     return db.execute_query(f"SELECT * FROM Albums_Songs;").fetchall()
 
+def add_albums_song(album_id, song_id) -> None:
+    """ Inserts a new Albums_Songs entity with the given album_id and song_id """
+    db.execute_query(f"INSERT INTO Albums_Songs (album_id, song_id) VALUES ('{int(album_id)}', '{int(song_id)}')")
+
+def delete_albums_song(albums_song_id) -> None:
+    """ Deletes an Albums_Songs entity using its unique albums_song_id """
+    db.execute_query(f"DELETE FROM Albums_Songs WHERE albums_song_id = {albums_song_id}")
+
+def check_albums_song_exists(album_id, song_id) -> int:
+    """
+    Checks if an Albums_Song with the given album_id and song_id already exists.
+    Returns 0 if it does not exist, and returns 1 if it does exist.
+    """
+    query = f"SELECT EXISTS(SELECT * FROM Albums_Songs WHERE album_id = {album_id} AND song_id = {song_id})"
+    return list((db.execute_query(query).fetchone()).values())[0]
+
+##################################################
+#                                                #
+# ARTISTS QUERIES                                #
+#                                                #
+##################################################
+
 def get_all_artists():
-    """  """
+    """ Returns aliased information about all Artists """
     return db.execute_query(("SELECT Artists.artist_id AS 'Artist ID', Artists.name AS Artist FROM Artists"
                              " ORDER BY Artists.name ASC;")).fetchall()
 
 def get_artist_names():
-    """  """
+    """ Returns all name values from the Artists table """
     return db.execute_query("SELECT name FROM Artists;").fetchall()
 
+##################################################
+#                                                #
+# SONG QUERIES                                   #
+#                                                #
+##################################################
+
 def get_all_songs():
-    """  """
+    """ Returns aliased information about all Songs """
     return db.execute_query(("SELECT Songs.song_id AS 'Song ID', Songs.song_title AS Song, Artists.name AS Artist, Songs.song_genre AS Genre, Songs.avg_song_rating AS 'Average Rating' FROM Songs"
                              " JOIN Artists ON Artists.artist_id = Songs.artist_id"
                              " ORDER BY Songs.song_title ASC;")).fetchall()
 
 def get_song_titles():
-    """  """
+    """ Returns all song_title values from the Songs table """
     return db.execute_query("SELECT song_title FROM Songs;").fetchall()
 
+def get_song_id_from_title(title) -> int:
+    """ Returns the corresponding song_id given a song_title """
+    return int(db.execute_query(f"SELECT song_id FROM Songs WHERE song_title = ('{title}')").fetchone()['song_id'])
+
+##################################################
+#                                                #
+# USER QUERIES                                   #
+#                                                #
+##################################################
+
 def get_all_users():
-    """  """
+    """ Returns aliased information about all Users """
     return db.execute_query(("SELECT Users.user_id AS 'User ID', username AS User, email AS 'E-mail' FROM Users"
                              " ORDER BY username ASC;")).fetchall()
+def get_usernames():
+    """ Returns all username values from the Users table """
+    return db.execute_query("SELECT username FROM Users;").fetchall()
 
-def get_add_review_info(review_type):
-    """  """
-    reviews = db.execute_query(f"SELECT {review_type}_title FROM {review_type.capitalize()}s;").fetchall()
-    users = db.execute_query("SELECT username FROM Users")
+def add_user(username, email) -> None:
+    """ Inserts a new User entity with the given username and email """
+    db.execute_query(f"INSERT INTO Users (username, email) VALUES ('{username}', '{email}')")
 
-    return (reviews, users)
+def delete_user(user_id) -> None:
+    """ Deletes a User entity using its unique user_id """
+    db.execute_query(f"DELETE FROM Users WHERE user_id = {user_id}")
+
+##################################################
+#                                                #
+# GENERAL QUERIES                                #
+#                                                #
+##################################################
 
 def get_star_entity(entity_name):
     """  """
     return db.execute_query(f"SELECT * FROM {entity_name.capitalize()}s;").fetchall()
+
+def get_star_entity_where_id(entity_name, id):
+    """ Gets all of the rows from entity_name's table where the entity's primary key ID matches id """
+    return db.execute_query(f"SELECT * FROM {entity_name}s WHERE {entity_name}_id = {id}").fetchall()
